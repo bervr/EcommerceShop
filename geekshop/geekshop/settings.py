f41dc193881dbfb3d2fb3b9a0eae2ea47f529b40
philippov <bervr@mail.ru>
2021-07-13 15:35:03 +0400

    static root

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -156,6 +156,8 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
+STATIC_ROOT = (os.path.join(BASE_DIR,'/static/'))
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
