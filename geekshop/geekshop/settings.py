97d73ba210d41e61ce3efdbac5250a8883ec9ce9
philippov <bervr@mail.ru>
2021-07-18 07:55:48 +0400

    some fixed

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -95,20 +95,20 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'geekshop',
-#         'USER': 'postgres'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'geekshop',
+        'USER': 'postgres'
+    }
+}
 
 
 # Password validation
@@ -148,10 +148,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "geekshop","static"),
-    os.path.join(BASE_DIR, "basketapp","static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "geekshop","static"),
+#     os.path.join(BASE_DIR, "basketapp","static"),
+# )
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
